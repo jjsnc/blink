b201e73f22e7ade95a68c9435d9b6a63cf9c8583
Damon <zhuowangyi@163.com>
2019-12-01 14:26:13 +0800

    http 封装

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  api_base_url: 'http://bl.7yue.pro/v1/',
+  appkey: "eM6FrYXNzYd9vDIR",
+}
+
+
+
+export {
+  config
+}
--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -16,6 +16,9 @@ Page({
        url: 'http://bl.7yue.pro/v1/classic/latest',
        header:{
          appkey:"eM6FrYXNzYd9vDIR"
+       },
+       success:(res)=> {
+         console.log(res,'res')
        }
      })
   },
