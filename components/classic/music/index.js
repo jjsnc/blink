1b8cab0f82d2b30174d33b5144deb28b57a13cf2
Damon <zhuowangyi@163.com>
2019-12-02 11:59:47 +0800

    music init

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,17 +1,24 @@
-// components/classic/music/index.js
+
+import {
+  classicBeh
+} from '../classic-beh.js'
 Component({
   /**
    * 组件的属性列表
    */
+  behaviors: [classicBeh],
   properties: {
-
+    src: String,
+    title: String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    playing: false,
+    pauseSrc: './images/player@pause.png',
+    playSrc: './images/player@play.png',
   },
 
   /**
