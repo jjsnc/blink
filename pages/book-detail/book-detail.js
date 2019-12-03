4dd425fbd3243f4efd1b0b28582e717605991b2b
Damon <zhuowangyi@163.com>
2019-12-03 10:57:01 +0800

    book-detail

--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -1,18 +1,45 @@
 // pages/book-detail/book-detail.js
+import {
+  BookModel
+} from '../../models/book.js'
+import {
+  LikeModel
+} from '../../models/like.js'
+const bookModel = new BookModel()
+const likeModel = new LikeModel()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    comments: [],
+    book: null,
+    likeStatus: false,
+    likeCount: 0,
+    posting: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options,'options')
+    wx.showLoading()
+    const bid = options.bid
+    const detail = bookModel.getDetail(bid)
+    const comments = bookModel.getComments(bid)
+    const likeStatus = bookModel.getLikeStatus(bid)
+
+    Promise.all([detail, comments, likeStatus])
+      .then(res => {
+        this.setData({
+          book: res[0],
+          comments: res[1].comments,
+          likeStatus: res[2].like_status,
+          likeCount: res[2].fav_nums
+        })
+        wx.hideLoading()
+      })
 
   },
 
