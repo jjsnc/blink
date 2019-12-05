a164318b27649886b742f31206add12938031341
Damon <zhuowangyi@163.com>
2019-12-05 11:37:31 +0800

    search

--- components/search/index.js
+++ components/search/index.js
@@ -11,13 +11,45 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    historyWords: [],
+    hotWords: [],
+    searching: false,
+    q: '',
+    loading: false,
+    loadingCenter: false
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    onCancel(event) {
+      console.log(event, 'onCancel')
+      // this.initialize()
+      // this.triggerEvent('cancel', {}, {})
+    },
+    onDelete(event) {
+      // this.initialize()
+      // this._closeResult()
+      console.log(event, 'onDelete')
+    },
+    onConfirm(event) {
+      console.log(event, 'event')
+      // this._showResult()
+      // this._showLoadingCenter()
+      // // this.initialize() 
+      // const q = event.detail.value || event.detail.text
+      // this.setData({
+      //   q
+      // })
+      // bookModel.search(0, q)
+      //   .then(res => {
+      //     this.setMoreData(res.books)
+      //     this.setTotal(res.total)
+      //     keywordModel.addToHistory(q)
+      //     this._hideLoadingCenter()
+      //   })
+    }
 
   }
 })
